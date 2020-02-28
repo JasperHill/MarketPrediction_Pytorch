62e595c6cc3f45073d5a42f639f699ed3516e343
JasperH <jasperlhill@gmail.com>
2020-02-27 20:38:23 -0500

    fixed bugs

--- Train_RNN.py
+++ Train_RNN.py
@@ -9,6 +9,8 @@ import os
 
 sys.path.append(os.path.join(os.getcwd(),'build_tools'))
 
+import time
+import random
 import pathlib
 import MODEL_CONSTANTS
 import BPI_RNN
@@ -27,6 +29,8 @@ from torch.utils.data      import Dataset, DataLoader
 
 """
 
+random.seed(time.time())
+
 #########################################################################
 ##  preprocess data
 #########################################################################
@@ -44,13 +48,31 @@ dates          =             np_object[:,0]
 prices_raw     =             np.asarray(np_object[:,1:], dtype=np.float64)
 
 ## express each element as the relative difference between neighboring prices
-prices_div      =           np.asarray(prices_raw[:-1])
+prices_div      =            np.asarray(prices_raw[:-1])
 prices_num      =            np.asarray(prices_raw[1:])
-prices           =            np.divide(prices_num, prices_div) - 1
+prices          =            np.divide(prices_num, prices_div) - 1
 
-## convert array to torch tensors
-prices = torch.from_numpy(prices)
+## convert array to torch tensors and reshape to (high/low, timestep)
+prices_pt       =            torch.transpose(torch.from_numpy(prices), 0, 1)
+ref_prices_pt   =            torch.transpose(torch.from_numpy(prices_raw), 0, 1)
 
+
+def integrate_output(ref_prices_pt, ref_idxs, output):
+    #create high/low reference prices
+    ref_prices = ref_prices_pt[:,ref_idxs[0]]
+    
+    y = torch.empty_like(output)
+    timesteps = output.shape[-1]
+
+    for i in range(timesteps):
+        prices = ref_prices * (output[:,i]+1)
+        ref_prices = prices
+        y[:,i] = prices
+
+    return y
+
+def percent_error(pred_vals, true_vals):
+    f = 100*(pred_vals-true_vals)/true_vals
 
 class BTC_Dataset(Dataset):
     def __init__(self, data, START, END, history_size, target_size, overlap):
@@ -58,54 +80,51 @@ class BTC_Dataset(Dataset):
         self.history_size = history_size
         self.target_size = target_size
 
-        self.ds = []
+        self.start = START + history_size
+        self.overlap = overlap
+        self.data = data
 
-        if END is None: END = len(data)
-
+        if END is None: END = len(self.data[0])
         END -= target_size-overlap
-        
-        for i in range(START+history_size,END):
-            x_idxs = range(i-history_size, i)
-            x = data[x_idxs]
 
-            y_idxs = range(i-overlap, i-overlap+target_size)
-            y = data[y_idxs]
-            
-            self.ds.append([x,y])
+        self.end = END
         
     def __len__(self):
-        return len(self.ds)
+        return self.end-self.start
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx): idx = idx.tolist()
-        x, y = self.ds[idx][0], self.ds[idx][1]
-        x, y = torch.tensor(x), torch.tensor(y)
         
-        return {'x': x, 'y': y}
+        idx += self.history_size
+        x_idxs = range(idx-self.history_size, idx)
+        x = self.data[:,x_idxs]
+        
+        y_idxs = range(idx-self.overlap, idx-self.overlap+self.target_size)
+        y_idxs = torch.tensor(y_idxs)
+        y = self.data[:,y_idxs]
+
+        return {'x': x, 'y': y, 'y_idxs': y_idxs}
     
 ## auxiliary plotting function for visualization
-def plot_data(arrays, delta, title, filename):
+def plot_data(true_values, predictions, idxs, hist_size, targ_size, overlap, title, filename):
     fig = plt.figure(figsize=(10,10))
     labels = [['True high', 'True low'], ['Predicted high', 'Predicted low']]
     colors = [['blue','red'], ['green','yellow']]
-    
-    past = arrays[0].shape[0]
-    future = list(range(delta))
-    time_steps = range(-past,0) 
+
+    ref_range = range(idxs[0]-hist_size, idxs[-1])
     plt.title(title)
 
-    for i,x in enumerate(arrays):
-        if i:
-            ## array indices are (list index,time_step,high/low)
-            plt.plot(future, arrays[i][:,0], color=colors[i][0], marker='.', markersize=1, label=labels[i][0])
-            plt.plot(future, arrays[i][:,1], color=colors[i][1], marker='.', markersize=1, label=labels[i][1])
+    ## plot true values
+    plt.plot(ref_range, true_values[0][ref_range], color=colors[0][0], marker='.', markersize=1, label=labels[0][0])
+    plt.plot(ref_range, true_values[1][ref_range], color=colors[0][1], marker='.', markersize=1, label=labels[0][1])
+    print('idxs shape: {} | predictions shape: {}'.format(idxs.shape, predictions[0].shape))
+    ## plot predicted values
+    plt.plot(idxs, predictions[0], color=colors[1][0], marker='.', markersize=1, label=labels[1][0])
+    plt.plot(idxs, predictions[1], color=colors[1][1], marker='.', markersize=1, label=labels[1][1])
 
-        else:
-            plt.plot(future, arrays[i][:,0], color=colors[i][0], marker='.', markersize=1, label=labels[i][0])
-            plt.plot(future, arrays[i][:,1], color=colors[i][1], marker='.', markersize=1, label=labels[i][1])
-
+    
     plt.legend()
-    plt.xlim(xmin=time_steps[0], xmax=(delta+5)*2)
+    plt.xlim(xmin=idxs[0]-hist_size, xmax=(idxs[-1]+5))
     plt.xlabel('time step (d)')
     plt.savefig(filename+'.pdf')        
     plt.close(fig)
@@ -117,22 +136,21 @@ def plot_data(arrays, delta, title, filename):
 #########################################################################
 
 train_frac  =                               0.6
-LENGTH      =                       len(prices)
+LENGTH      =                 len(prices_pt[0])
 START_IDX   =                                 0
 TRAIN_SPLIT =   int(np.ceil(train_frac*LENGTH))
 HIST_SIZE   =         MODEL_CONSTANTS.HIST_SIZE
 TARG_SIZE   =         MODEL_CONSTANTS.TARG_SIZE
-HIDDEN_SIZE =       MODEL_CONSTANTS.HIDDEN_SIZE
-OVERLAP     =       int(np.ceil(0.5*HIST_SIZE))
+OVERLAP     =                                 0
 
 BATCH_SIZE  =        MODEL_CONSTANTS.BATCH_SIZE
 BUFFER_SIZE =                              1000
 NUM_WORKERS =                                 1
-EPOCHS           =                                                   range(100)
+EPOCHS      =                          range(1)
 
 
-train_ds = BTC_Dataset(prices, START_IDX, TRAIN_SPLIT, HIST_SIZE, TARG_SIZE, OVERLAP)
-test_ds  = BTC_Dataset(prices, TRAIN_SPLIT, None, HIST_SIZE, TARG_SIZE, OVERLAP)
+train_ds = BTC_Dataset(prices_pt, START_IDX, TRAIN_SPLIT, HIST_SIZE, TARG_SIZE, OVERLAP)
+test_ds  = BTC_Dataset(prices_pt, TRAIN_SPLIT, None, HIST_SIZE, TARG_SIZE, OVERLAP)
 
 train_dl = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 test_dl = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
@@ -146,15 +164,16 @@ test_size = len(train_dl)
 
 LSTM_MODEL_SAVE_PATH = "./build_tools/lstm_saves.pth"
 
-lstm_model = BPI_RNN.LSTM_Model(BATCH_SIZE, HIST_SIZE, TARG_SIZE, HIDDEN_SIZE)
+lstm_model = BPI_RNN.LSTM_Model(BATCH_SIZE, 2, HIST_SIZE, 2, TARG_SIZE)
 lstm_model.load_state_dict(torch.load(LSTM_MODEL_SAVE_PATH))
 
 optimizer = optim.Adam(lstm_model.parameters())
 
+final_ys, ref_idxs = None, None
 train_MSE_hist = []
 test_MSE_hist  = []
 
-status_labels = ['training loss', 'testing loss']
+status_labels = ['training loss', 'testing loss', 'epoch CPU time (min)']
 
 print('|{: ^10}'.format('epoch'), end='')
 for lbl in status_labels:
@@ -162,6 +181,8 @@ for lbl in status_labels:
 print('|')
 
 for epoch in EPOCHS:
+    start = time.time()
+    
     train_loss = 0.0
     test_loss = 0.0
 
@@ -169,31 +190,49 @@ for epoch in EPOCHS:
         x, y = data['x'], data['y']
 
         optimizer.zero_grad()
-        y_pred = lstm_model(x)
+        y_pred = lstm_model(x).to(torch.double)
         loss = F.mse_loss(y_pred, y, reduction='mean')
 
-        loss.backward()
+        loss.backward(retain_graph=True)
         optimizer.step()
 
         train_loss += loss.item()
     train_MSE_hist.append(train_loss/train_size)
 
     with torch.no_grad():
+        i = 0
+        
+        if (epoch == EPOCHS[-1]):
+            j = random.randrange(0,len(test_dl))
+            print('j = ',j)            
+        
         for data in test_dl:
-            x, y = data['x'], data['y']
+            x, y, y_idxs = data['x'], data['y'], data['y_idxs']
 
             y_pred = lstm_model(x)
             loss = F.mse_loss(y_pred, y, reduction='mean')
-
             test_loss += loss.item()
+            
+            if ((epoch == EPOCHS[-1]) and (i == j)):
+                final_pred = y_pred[-1]
+                ref_idxs = y_idxs[-1]
+            i += 1
 
         test_MSE_hist.append(test_loss/test_size)
 
-    nums = [train_MSE_hist[-1], test_MSE_hist[-1]]
+    end = time.time()
+    duration = end-start
+    
+    nums = [train_MSE_hist[-1], test_MSE_hist[-1], duration/(60*(epoch+1))]
     print('|{: >10}'.format(epoch), end='')
     for num in nums:
-        print('|{: >30.3f}'.format(num), end='')
+        print('|{: >30.3e}'.format(num), end='')
     print('|')
+
+final_y = integrate_output(ref_prices_pt, ref_idxs, final_pred)
+plot_data(ref_prices_pt, final_y, ref_idxs, HIST_SIZE, TARG_SIZE, OVERLAP, 'Bitcoin Price Over Time', 'Network_Prediction')
+
+pe = percent_error(final_y, ref_prices_pt[:,ref_idxs])
 
 plt.figure(num=0, figsize=(10,10))
 plt.plot(EPOCHS, np.asarray(train_MSE_hist), c='blue', linestyle='-', label='training loss')
@@ -204,3 +243,5 @@ plt.title('MSE over epochs')
 plt.legend()
 plt.savefig('RNN_MSE.pdf')
 plt.close(0)
+
+torch.save(lstm_model.state_dict(), LSTM_MODEL_SAVE_PATH)
--- build_tools/BPI_RNN.py
+++ build_tools/BPI_RNN.py
@@ -22,30 +22,32 @@ save = options.save
 
 HIST_SIZE        = MODEL_CONSTANTS.HIST_SIZE
 TARG_SIZE        = MODEL_CONSTANTS.TARG_SIZE
-HIDDEN_SIZE      = MODEL_CONSTANTS.HIDDEN_SIZE
 BATCH_SIZE       = MODEL_CONSTANTS.BATCH_SIZE
 
 class LSTM_Model(nn.Module):
-    def __init__(self, batch_size, input_dim, output_dim, hidden_size):
+    def __init__(self, batch_size, input_channels, input_dim, output_channels, output_dim):
         super(LSTM_Model, self).__init__()
+        self.input_channels = input_channels
         self.input_dim = input_dim
+        
         self.output_dim = output_dim
-        self.hidden_size = hidden_size
+        self.output_channels = output_channels
         self.batch_size = batch_size
 
-        self.lstm_op = LSTM_Op(batch_size, input_dim, output_dim, hidden_size)
-        self.linear  = nn.Linear(in_features=(output_dim+hidden_size), out_features=output_dim)
+        self.lstm_op = LSTM_Op(batch_size, input_channels, input_dim, output_channels, output_dim)
+        #self.linear  = nn.Linear(in_features=output_dim, out_features=output_dim)
         self.tanh    = nn.Tanh()
 
     def forward(self, x):
         x = self.lstm_op(x)
-        x = self.linear(x)
+        #x = self.linear(x)
 
         return self.tanh(x)
 
 LSTM_MODEL_SAVE_PATH = "./lstm_saves.pth"
 
-lstm_model = LSTM_Model(BATCH_SIZE, HIST_SIZE, TARG_SIZE, HIDDEN_SIZE)
+# 2 input and output channels for high/low prices
+lstm_model = LSTM_Model(BATCH_SIZE, 2, HIST_SIZE, 2, TARG_SIZE)
 
 if save:
     torch.save(lstm_model.state_dict(), LSTM_MODEL_SAVE_PATH)
